da12f8e165b39794dfd028de8323a238ec87ed70
Joseph Ojo <70476696+OjoFemiJoseph@users.noreply.github.com>
2024-09-13 21:36:21 +0000

    new

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y nodejs npm
-sudo npm install pm2 -g
-sudo npm install express
-sudo rm -rf /home/ec2-user/my-app1
+yum install -y httpd
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Stop all servers and start the server
-sudo pm2 stop all
-sudo pm2 start /home/ec2-user/my-app1/index.js
+service httpd start
